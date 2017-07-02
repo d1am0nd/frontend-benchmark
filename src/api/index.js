40635e985d1313600d979c7feae4b859e666ffbd
devk <devkordesg@gmail.com>
2017-07-03 01:04:40 +0200

    paths corrected

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
-const LUMEN_PREFIX = 'http://lumenapi.myprogramming.blog/'
-const GOLANG_PREFIX =  'http://goapi.myprogramming.blog/'
+const LUMEN_PREFIX = 'http://lumenapi.myprogramming.blog'
+const GOLANG_PREFIX =  'http://goapi.myprogramming.blog'
 
 const LUMEN_SINGLE_ROW_URL = LUMEN_PREFIX + '/api/first_db_result'
 const LUMEN_MULTI_ROW_URL = LUMEN_PREFIX + '/api/all_db_results'
@@ -22,7 +22,11 @@ export default {
     return Vue.http.get(LUMEN_STATIC_URL)
   },
   getGolangSingle () {
-    return Vue.http.get(GOLANG_SINGLE_ROW_URL)
+    return Vue.http.get(GOLANG_SINGLE_ROW_URL, null, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
   },
   getGolangMulti () {
     return Vue.http.get(GOLANG_MULTI_ROW_URL)
